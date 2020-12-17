9039ee160a1d145fef6ea609bf6cfaf7b6576b46
umanochiocciola <59803009+umanochiocciola@users.noreply.github.com>
2020-12-17 10:25:47 +0100

    Update server_index.py

--- ServerDemo/server_index.py
+++ ServerDemo/server_index.py
@@ -1,4 +1,6 @@
-# EDIT THIS FILE
 main = {
-    'index': 'Welcome to the new Internet!\nNO cookies\nNO ads\nNO property of big companies!\n\nFree, Decentralized, untracked'
+    'index': 'Welcome to the new Internet!\nNO cookies\nNO ads\nNO property of big companies!\n\nFree, Decentralized, untracked\n\n[about] [how to setup a server] [help]',
+    'about': 'This project is intended to create a net of decentralized servers, a free place to do whatever you want, without big companies spying us.\nSomething like what the internet was meant to be at the beginning.',
+    'how to setup a server': 'Setting up your own server is pretty easy:\n1) download sserver from github (https://github.com/umanochiocciola/OurNet/tree/main/ServerDemo)\n2) edit server_index.py: the main dictionary is your "website folder"\n3) open a terminal and type\npython3 server.py 1234\n(replacing 1234 with a valid port)',
+    'help': 'to connect to a server start client.py and input ip and port of the server.\nThen you can send a request to the server to get specific pages'
 }
